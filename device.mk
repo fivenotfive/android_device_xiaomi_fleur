dedc3de50363e6e490479731aed3c833925f1925
Vaisakh Murali <mvaisakh@statixos.com>
2022-11-13 08:43:49 +0300

    fleur: Build libpcap from source
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: I6de4b51b76c7fcdccb06036200f6992893da6b34
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -193,6 +193,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Pcap
+PRODUCT_PACKAGES += \
+    libpcap.vendor
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
