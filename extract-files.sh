05c31ee390dc00354086c820f4c22ef15cbaddec
fjrXTR <fajarslebew31@gmail.com>
2023-11-02 13:59:11 +0000

    fleur/extract_files: Rebrand to mt6781
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -55,11 +55,11 @@ fi
 
 function blob_fixup {
     case "$1" in
-        vendor/lib*/hw/audio.primary.mt6785.so)
+        vendor/lib*/hw/audio.primary.mt6781.so)
             "${PATCHELF}" --add-needed "libshim_audio.so" "${2}"
             "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v30.so" "${2}"
             ;;
-        vendor/lib*/hw/audio.usb.mt6785.so)
+        vendor/lib*/hw/audio.usb.mt6781.so)
             "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v30.so" "${2}"
             ;;
         vendor/lib64/libwifi-hal-mtk.so)
@@ -68,7 +68,7 @@ function blob_fixup {
         vendor/lib64/libmtkcam_stdutils.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
-        vendor/lib*/hw/dfps.mt6785.so)
+        vendor/lib*/hw/dfps.mt6781.so)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
             ;;
         vendor/lib*/hw/vendor.mediatek.hardware.pq@2.6-impl.so)
